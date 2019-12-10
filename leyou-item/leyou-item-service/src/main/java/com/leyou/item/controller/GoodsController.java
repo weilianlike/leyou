042b1b9c93444d1032a913301ec636ea809c49c7
weilianlike <617693751@qq.com>
2019-12-10 13:58:04 +0800

    商城完整后台代码--最终版

--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/GoodsController.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/GoodsController.java
@@ -2,16 +2,17 @@ package com.leyou.item.controller;
 
 import com.leyou.common.pojo.PageResult;
 import com.leyou.item.bo.SpuBo;
+import com.leyou.item.pojo.Sku;
+import com.leyou.item.pojo.SpuDetail;
 import com.leyou.item.service.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class GoodsController {
@@ -48,6 +49,7 @@ public class GoodsController {
      *
      * @param
      * @return void
+     *
      */
     @PostMapping("goods")
     //通过@RequestBody注解来接收Json请求
@@ -56,4 +58,53 @@ public class GoodsController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    /**
+     *修改商品接口
+     * @param
+     * @return void
+     *- 请求方式：PUT
+     * - 请求路径：/
+     * - 请求参数：Spu对象
+     * - 返回结果：无
+     */
+    @PutMapping("goods")
+    public ResponseEntity<Void> updateGoods(@RequestBody SpuBo spuBo){
+        this.goodsService.update(spuBo);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+
+    /**
+     *查询SpuDetail接口
+     * @param
+     * @return void
+     * - 请求方式：GET
+     * - 请求路径：/spu/detail/{id}
+     * - 请求参数：id，应该是spu的id
+     * - 返回结果：SpuDetail对象
+     */
+    @GetMapping("spu/detail/{spuId}")
+    public ResponseEntity<SpuDetail> querySpuDetailBySpuId(@PathVariable("spuId")Long spuId){
+        SpuDetail spuDetail = this.goodsService.querySpuDetailBySpuId(spuId);
+        if (spuDetail == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(spuDetail);
+    }
+    /**
+     *查询SpuDetail接口
+     * @param
+     * - 请求方式：Get
+     * - 请求路径：/sku/list
+     * - 请求参数：id，应该是spu的id
+     * - 返回结果：sku的集合
+     */
+    @GetMapping("sku/list")
+    public ResponseEntity<List<Sku>> querySkusBySpuId(@RequestParam("id")Long spuId){
+        List<Sku> skus = this.goodsService.querySkusBySpuId(spuId);
+        if (CollectionUtils.isEmpty(skus)) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(skus);
+    }
 }
